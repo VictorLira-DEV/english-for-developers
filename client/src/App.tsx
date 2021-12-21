0644ee7ba36302b1f06c8a3b5e6446f2ed04cc7c
VictorLira-DEV <victorliradev@gmail.com>
2021-12-21 20:47:26 -0200

    lazy loading with React.lazy

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,19 @@
-import Home from './pages/home/Home';
+import React, { Suspense } from 'react';
 import Header from './components/header/Header';
 import './global/styles/index.css';
 import { Route, Switch, Redirect } from 'react-router-dom';
-import Expressions from './pages/expressions/Expressions';
-import PhrasalVerbs from './pages/phrasalVerbs/PhrasalVerbs';
-import Login from './pages/login/Login';
-import Registration from './pages/registration/Registration';
-import Profile from './pages/profile/Profile';
 import { useContext } from 'react';
 import { AuthContext } from './context/auth-context/auth-context';
+import LoadSpinner from './components/load-spinner/LoadSpinner';
+
+const Home = React.lazy(() => import('./pages/home/Home'));
+const Login = React.lazy(() => import('./pages/login/Login'));
+const Expressions = React.lazy(() => import('./pages/expressions/Expressions'));
+const Profile = React.lazy(() => import('./pages/profile/Profile'));
+const Registration = React.lazy(
+    () => import('./pages/registration/Registration')
+);
+const PhrasalVerbs = React.lazy(() => import('./pages/phrasalVerbs/PhrasalVerbs'))
 
 function App() {
     const authCtx = useContext(AuthContext);
@@ -16,35 +21,43 @@ function App() {
     return (
         <div className="app">
             <Header />
-            <Switch>
-                <Route path="/" exact>
-                    <Home />
-                </Route>
-                <Route path="/phrasal-verbs">
-                    <PhrasalVerbs />
-                </Route>
-                {authCtx.isLoggedIn && (
-                    <Route path="/expressions">
-                        <Expressions />
+            <Suspense
+                fallback={
+                    <div className="centered">
+                        <LoadSpinner />
+                    </div>
+                }
+            >
+                <Switch>
+                    <Route path="/" exact>
+                        <Home />
                     </Route>
-                )}
-                {!authCtx.isLoggedIn && (
-                    <Route path="/login">
-                        <Login />
+                    <Route path="/phrasal-verbs">
+                        <PhrasalVerbs />
                     </Route>
-                )}
-                <Route path="/signup">
-                    <Registration />
-                </Route>
-                {authCtx.isLoggedIn && (
-                    <Route path="/profile">
-                        <Profile />
+                    {authCtx.isLoggedIn && (
+                        <Route path="/expressions">
+                            <Expressions />
+                        </Route>
+                    )}
+                    {!authCtx.isLoggedIn && (
+                        <Route path="/login">
+                            <Login />
+                        </Route>
+                    )}
+                    <Route path="/signup">
+                        <Registration />
                     </Route>
-                )}
-                <Route path="*">
-                    <Redirect to="/" />
-                </Route>
-            </Switch>
+                    {authCtx.isLoggedIn && (
+                        <Route path="/profile">
+                            <Profile />
+                        </Route>
+                    )}
+                    <Route path="*">
+                        <Redirect to="/" />
+                    </Route>
+                </Switch>
+            </Suspense>
         </div>
     );
 }
