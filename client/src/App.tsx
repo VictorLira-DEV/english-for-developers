ddd30f0d68c7e52583f48b2419702317c2180b30
VictorLira-DEV <victorliradev@gmail.com>
2022-03-30 21:40:06 -0300

    lazy loading removed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,15 +5,22 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import { useContext } from 'react';
 import { AuthContext } from './context/auth-context/auth-context';
 import LoadSpinner from './components/LoadSpinner';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Expressions from './pages/Expressions';
+import Profile from './pages/Profile';
+import Registration from './pages/Registration';
+import PhrasalVerbs from './pages/PhrasalVerbs';
+//LAZY LOADING
 
-const Home = React.lazy(() => import('./pages/Home'));
-const Login = React.lazy(() => import('./pages/Login'));
-const Expressions = React.lazy(() => import('./pages/Expressions'));
-const Profile = React.lazy(() => import('./pages/Profile'));
-const Registration = React.lazy(
-    () => import('./pages/Registration')
-);
-const PhrasalVerbs = React.lazy(() => import('./pages/PhrasalVerbs'))
+// const Home = React.lazy(() => import('./pages/Home'));
+// const Login = React.lazy(() => import('./pages/Login'));
+// const Expressions = React.lazy(() => import('./pages/Expressions'));
+// const Profile = React.lazy(() => import('./pages/Profile'));
+// const Registration = React.lazy(
+//     () => import('./pages/Registration')
+// );
+// const PhrasalVerbs = React.lazy(() => import('./pages/PhrasalVerbs'))
 
 function App() {
     const authCtx = useContext(AuthContext);
